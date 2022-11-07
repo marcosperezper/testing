9d70b88c5c2a98060f433080f3669b372917d4f5
marcos <marcosperez@IT-47-Marcos-Perez-Perpina.local>
2022-11-07 15:31:56 +0100

    Lesson 2 plain-asserts

---
+++ Lesson2/plain-asserts/test_long_dictionaries.py
@@ -0,0 +1,4 @@
+def test_long_dictionaries():
+    result = {'key':'value', 'lastname':'perez', 'name':'marcos'}
+    expected = {'key':'value', 'lastname':'perez', 'name':'marcos'}
+    assert result == expected
---
+++ Lesson2/plain-asserts/test_long_string.py
@@ -0,0 +1,5 @@
+long_string = "This is a very long string that needs to be compared." 
+
+def test_long_string():
+    expected = "This is A very long string that needs to be compared." 
+    assert long_string == expected
